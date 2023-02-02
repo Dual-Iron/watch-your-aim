ddf031f2c95e20b816cdf71476560f14878a72e9
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-02-02 00:42:36 -0500

    Set up mod

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,28 +1,30 @@
-﻿global using System;
-global using System.Linq;
-global using System.Collections.Generic;
-global using UnityEngine;
-
-using BepInEx;
+﻿using BepInEx;
+using System;
 using System.Security.Permissions;
+using UnityEngine;
 
 #pragma warning disable CS0618 // Do not remove the following line.
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
-namespace TestMod;
+namespace WatchYourAim;
 
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+[BepInPlugin("com.dual.watch-your-aim", "Watch Your Aim", "0.1.0")]
 sealed class Plugin : BaseUnityPlugin
 {
     public void OnEnable()
     {
-        On.RainWorld.OnModsInit += Init;
+        On.ScavengerAI.IsThrowPathClearFromFriends += ScavengerAI_IsThrowPathClearFromFriends;
     }
 
-    private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    private bool ScavengerAI_IsThrowPathClearFromFriends(On.ScavengerAI.orig_IsThrowPathClearFromFriends orig, ScavengerAI self, Vector2 throwPos, float margin)
     {
-        orig(self);
+        return orig(self, throwPos, margin) && !Obstructed(self, throwPos, margin);
+    }
 
-        Logger.LogDebug("Hello world!");
+    private bool Obstructed(ScavengerAI self, Vector2 to, float margin)
+    {
+        Vector2 from = self.scavenger.mainBodyChunk.pos;
+
+        throw new NotImplementedException();
     }
 }
